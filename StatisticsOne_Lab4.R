b8365c02876123dec38585e33d0ac509fb7aa2c5
Stu Kerr <stu.v.kerr@gmail.com>
2013-10-15 13:58:50 -0700

    finished lab

--- StatisticsOne_Lab4.R
+++ StatisticsOne_Lab4.R
@@ -16,6 +16,8 @@ setwd("/Users/stuart/R_Files/Statistics Class")
 # If necessary install packages
 install.packages("psych")
 library(psych)
+library(ggplot2)  # installing ggplot2 for my own plot compare and contrast
+
 # Read data into a dataframe called PE (physical endurance)
 PE = read.table("Stats1.13.Lab.04.txt", header=T)
 # Summary statistics
@@ -29,15 +31,55 @@ cor.test(PE$age, PE$activeyears)
 cor.test(PE$endurance, PE$activeyears)
 cor.test(PE$endurance, PE$age)
 
-# Histograms - to kinda check sense of cor.tests
+## Histograms - to kinda check sense of cor.tests
 layout(matrix(c(1,2,3,4),2,2,byrow = TRUE))
 hist(PE$age)
 hist(PE$activeyears)
 hist(PE$endurance)
 layout(matrix(c(1,1),1,1, byrow = TRUE))
 
-# Regression Analyses, unstandardized
+## Regression Analyses, UNSTANDARDIZED
 model1 <- lm(PE$endurance ~ PE$age)
 summary(model1)
-plot(PE$endurance ~ PE$activeyears, main = "Scatterplot", ylab = "Endurance", xlab = "Active Years")
+plot(PE$endurance ~ PE$activeyears, main = "Scatterplot", ylab = "Endurance", xlab = "Age")
 abline(lm(PE$endurance ~ PE$age), col="blue")
+
+model2 <- lm(PE$endurance ~ PE$activeyears)
+summary(model2)
+plot(PE$endurance ~ PE$activeyears, main = "Scatterplot" , ylab = "Endurance", xlab = "Active Years")
+abline(lm(PE$endurance ~ PE$activeyears), col="blue")
+## use ggplot for comparison
+sp <- ggplot(PE,aes(x=activeyears,y=endurance, colour=age))
+sp + geom_point(size=2.5)
+# now add a regression line
+sp + geom_point(size=2.5) + stat_smooth(method=lm, level=0.80)
+
+model3 <- lm(PE$endurance ~ PE$age + PE$activeyears) # technically this is a multiple regression
+summary(model3)
+
+# To visualize model3, save the predicted scores as a new variable and then plot with endurance
+# Plotting predicted scores takes in the multiple axes for predictors that cannot be plotted individually
+PE$predicted <- fitted(model3)
+plot(PE$endurance ~ PE$predicted, main = "scatterplot",ylab="Endurance",xlab="Model3 Predicted Scores")
+abline(lm(PE$endurance ~ PE$predicted),col="blue")
+# Save the model residuals
+PE$e <- resid(model3)
+hist(PE$e)
+plot(PE$predicted ~ PE$e, main = "Scatterplot",ylab="Model3 Predicted Scores",xlab = "Model 3 Residuals")
+abline(lm(PE$predicted ~ PE$e),col="blue")
+# Using ggplot2
+sp <- ggplot(PE,aes(x=PE$e,y=PE$predicted, colour=age))
+sp + geom_point(size=2.5)
+
+## Regression analyses, STANDARDIZED
+# In simple regression, the standardized regression coefficient will be same as the correlation coefficient
+round(cor(PE[2:4]),2)
+
+model1.z <- lm(scale(PE$endurance) ~ scale(PE$age))
+summary(model1.z)
+
+model2.z <- lm(scale(PE$endurance) ~ scale(PE$activeyears))
+summary(model2.z)
+
+model3.z <- lm(scale(PE$endurance) ~ scale(PE$age) + scale(PE$activeyears))
+summary(model3.z)
